30fca07baf7d726c9ffa3a9747712541b1dd514e
parzivalfowl@gmail.com <parzivalfowl@gmaiml.com>
2021-11-23 20:28:00 +0300

    higher level programming

---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,1 @@
+This is a python3 file list for alx
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-# 2-replace_in_list.py
+# 3-print_reversed_list_integer.py
 
 
-def replace_in_list(my_list, idx, element):
-    """Replace an element of a list at a specific position."""
-    if idx >= 0 and idx < len(my_list):
-        my_list[idx] = element
-    return (my_list)
+def print_reversed_list_integer(my_list=[]):
+    """Print all integers of a list in reverse order."""
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
